507c865a7025eea0031e62f51fd8eaa328028f92
shaikh-debian <skmudassirahmed@gmail.com>
2022-09-10 09:05:18 +0530

    CDN Setup

--- post/models.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime 
-from django.db import models
-
-class PostModel(models.Model):
-    title = models.CharField(max_length=200,default="")
-    content = models.TextField(default="")
-    created = models.DateTimeField(default=datetime.now, blank=True)
-    user = models.CharField(max_length=200, default="")
-
